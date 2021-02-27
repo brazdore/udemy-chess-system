b3be8baf08ae499ecbd5a04fb39c70c62bc15783
Brian dos Reis <brazdore@gmail.com>
2021-02-26 22:34:09 -0300

    Amarelo + Verde.

--- src/com/company/application/UI.java
+++ src/com/company/application/UI.java
@@ -26,18 +26,18 @@ public class UI {
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + " ");
+            System.out.print(ANSI_YELLOW + (8 - i) + "  ");
             for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j]);
             }
             System.out.println();
         }
-        System.out.println("  a b c d e f g h");
+        System.out.println(ANSI_YELLOW + "  a b c d e f g h");
     }
 
     private static void printPiece(ChessPiece piece) {
         if (piece == null) {
-            System.out.print("-");
+            System.out.print(ANSI_GREEN + "-");
         } else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_BLUE + piece + ANSI_RESET);
